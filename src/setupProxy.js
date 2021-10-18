1c75532941ccd04039b526d9828283e56997c2af
kwamekert <kwamekert@gmail.com>
2021-10-18 12:44:15 +0000

    update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,14 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api/v1/**",
+    "/api",
     createProxyMiddleware({
       target: "http://165.227.192.124:8080",
       changeOrigin: true,
       logLevel: "debug",
+      pathRewrite: {
+        [`^/api`]: "/api/v1",
+      },
     })
   );
 };
